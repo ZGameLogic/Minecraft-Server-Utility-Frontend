e6a8eaf9a1d6b9cf78d4af1d4fdc6be79f862f21
Ben Shabowski <benshabowski@gmail.com>
2023-11-28 17:17:22 -0600

    Websockets are almost there

--- src/pages/ServerDetailPage.tsx
+++ src/pages/ServerDetailPage.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from 'react-router-dom';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
 function ServerDetailPage() {
     const {server} = useParams();
+    const socket = new SockJS(`${process.env.REACT_APP_BACKEND_API_URL}/ws`);
+    const stompClient = Stomp.over(socket);
+
+    const [message, setMessage] = useState('Waiting for a message');
+
+    stompClient.connect({}, () => {
+        // stompClient.subscribe(`/app/server/${server}`, (message) => {
+        stompClient.subscribe('/**', (message) => {
+            console.log(message);
+            setMessage(message);
+        });
+    }, (error) => {
+        console.error('Stomp connection error:', error);
+    });
+
+    function sendMessage(message: object){
+        stompClient.send(`/app/server/${server}`, {}, JSON.stringify(message));
+    }
+
     return <>
         {server}
+        <button onClick={() => {
+            console.log('Stomp connection state:', stompClient.connected);
+            sendMessage({
+                action: 'none',
+                data: {}
+            });
+        }}>Test</button>
+        <p>{message}</p>
     </>;
 }
 
